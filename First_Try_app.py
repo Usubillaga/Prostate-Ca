05e4277ac7927767989b038dc3b2865d58910fcf
Usubillaga <79326179+Usubillaga@users.noreply.github.com>
2026-01-02 08:23:05 +0100

    Update First_Try_app.py

--- First_Try_app.py
+++ First_Try_app.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 # --- 1. Configuración de la Página ---
 st.set_page_config(
-    page_title="Prostate Cancer Algorithm 2026 (S3/EAU)",
+    page_title="Prostate Cancer Algorithm 2025 (S3/EAU)",
     layout="wide",
     initial_sidebar_state="expanded"
 )
